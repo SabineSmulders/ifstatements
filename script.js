8f41cfd4414708e133383ab214fa62d726724a3b
SabineSmulders <92474494+SabineSmulders@users.noreply.github.com>
2022-01-04 21:09:01 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+// if X, than Y 
+// if you click on banner X, we'll show you photo Y 
+
+const age = number; ("Hoe oud ben je?")
+if (age > 17) { 
+    
+    document.write("Je kan verder")
+
+} else {
+    document.write("Je kan geen ticket kopen")
+}
+
+const isFemale = Boolean
+if (isFemale = true){
+    
+    document.write("Je kan een ticket kopen")
+
+} else {
+    
+    document.write("Je kan helaas geen ticket kopen")
+}
+
+const driverStatus = designated
+if (driverStatus = designated) {
+
+    document.write("You're allowed to drive home")
+
+} else {
+
+    document.write("You're not allowed to drive")
+}
